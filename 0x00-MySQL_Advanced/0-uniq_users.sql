8ed752e2d69df95480d8071cd11ef7b1ac2d9bca
Jayneey <sauceqwinn@gmail.com>
2023-04-06 23:13:46 +0000

    uniq users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,5 @@
 -- Create a table with unique users.
-IF OBJECT_ID('users', 'U') IS NOT NULL
-    DROP TABLE users;
-
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
